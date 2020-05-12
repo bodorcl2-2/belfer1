efe8f6d1158d1d7133b9676dc948651228d3e3c8
darekrcl <darek.rcl@gmail.com>
2020-05-12 14:48:03 +0200

    Updates

---
+++ precache-manifest.60746ca241d789e37754214f45620e7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7389c1a6a47dff91f161e2e9fa25b7",
+    "url": "//bodorcl2-2.github.io/belfer1/index.html"
+  },
+  {
+    "revision": "bdf8da3844ed41a0a0d1",
+    "url": "//bodorcl2-2.github.io/belfer1/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bc9e179cef1b0f27010e",
+    "url": "//bodorcl2-2.github.io/belfer1/static/js/2.a1841def.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "//bodorcl2-2.github.io/belfer1/static/js/2.a1841def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf8da3844ed41a0a0d1",
+    "url": "//bodorcl2-2.github.io/belfer1/static/js/main.c61ad19b.chunk.js"
+  },
+  {
+    "revision": "8637a0f4a8c3b134822e",
+    "url": "//bodorcl2-2.github.io/belfer1/static/js/runtime-main.0707a2bc.js"
+  }
+]);
